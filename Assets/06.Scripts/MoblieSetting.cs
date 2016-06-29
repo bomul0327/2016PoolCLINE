5fef2bb4edd2fe38f42d42948b65106969248160
unknown <>
2016-06-29 19:03:57 +0900

    배경test

---
+++ Assets/06.Scripts/MoblieSetting.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoblieSetting : MonoBehaviour {
+    private bool exit = false;
+    private float time = 0.0f;
+
+    void Awake () {
+        Screen.orientation = ScreenOrientation.Portrait;
+        Screen.SetResolution(Screen.width, Screen.width * 16 / 9, true);
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
+    void Update () {
+        ExitApplication();
+    }
+
+    void ExitApplication () {
+        if (exit) {
+            time = +Time.deltaTime;
+            if(time >= 1f) {
+                exit = false;
+                time = 0f;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (!exit) {
+                exit = true;
+            }
+            else {
+                Application.Quit();
+            }
+        }
+    }
+}
