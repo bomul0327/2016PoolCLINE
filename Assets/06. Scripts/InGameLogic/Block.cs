2c64d9d1eb74731e1fb09221178366d0a2fb457e
황보 정 <bomul0327@naver.com>
2016-07-11 01:45:39 +0900

    메모리 풀 시스템, 인게임 틀잡기

---
+++ Assets/06. Scripts/InGameLogic/Block.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+    private int hp;
+    public int Hp {
+        get { return hp; }
+        set { hp = value; }
+    }
+}
