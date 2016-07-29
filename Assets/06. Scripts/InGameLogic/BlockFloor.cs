c1752395f1f79bb485a63cd4ad8c9f85b263a964
황보 정 <bomul0327@naver.com>
2016-07-30 04:14:38 +0900

    코드 정리

---
+++ Assets/06. Scripts/InGameLogic/BlockFloor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockFloor : MonoBehaviour {
+    private GameObject[] blockArray;
+    public GameObject[] BlockArray {
+        get {
+            return blockArray;
+        }
+    }
+    private Block[] block;
+    public Block[] Block {
+        get {
+            return block;
+        }
+    }
+
+	void Awake () {
+        blockArray = new GameObject[transform.childCount];
+        block = new Block[blockArray.Length];
+        for(int i = 0; i < blockArray.Length; i++) {
+            blockArray[i] = transform.GetChild(i).gameObject;
+            block[i] = blockArray[i].GetComponent<Block>();
+        }
+	}
+
+    public void SetBlocksBreakable(bool param) {
+        for(int i = 0; i < block.Length; i++) {
+            block[i].IsBreakable = param;
+        }
+    }
+
+    public void SetBlocksHp(int minRange, int maxRange) {
+        for(int i = 0; i < block.Length; i++) {
+            block[i].Hp = Random.Range(minRange, maxRange);
+        }
+    }
+
+    public void SetBlocksActive(bool param) {
+        for(int i = 0; i < block.Length; i++) {
+            blockArray[i].SetActive(param);
+        }
+    }
+}
--- Assets/06. Scripts/InGameLogic/GameManager.cs
+++ Assets/06. Scripts/InGameLogic/GameManager.cs
@@ -3,22 +3,17 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
     [SerializeField]
-    private Transform SpawnPos;
+    private Transform spawnPos;
     [SerializeField]
-    private Transform BreakablePos;
+    private Transform breakablePos;
     [SerializeField]
-    private Transform FadePos;
+    private Transform fadePos;
     [SerializeField]
-    private GameObject[] BlockFloor;
-
-    private bool isMoving = false;
-
-    //UI score
+    private BlockFloor[] blockFloor;
+    [SerializeField]
+    private float speed = 1.0f;
     [SerializeField]
     private InGameScore scoreUI;
-  
-
-    //To UI: IsPaused = true/false; 를 설정하게 하시면 됩니다.
 
     private bool isPaused = false;
     public bool IsPaused {
@@ -31,15 +26,10 @@ public class GameManager : MonoBehaviour {
     }
 
     void Start () {
-        for(int i = 0; i < BlockFloor[1].transform.childCount; i++) {
-            BlockFloor[1].transform.GetChild(i).GetComponent<Block>().IsBreakable = true;
-        }
-        for(int i = 0; i < BlockFloor.Length; i++) {
-            for(int j = 0; j < BlockFloor[i].transform.childCount; j++) {
-                BlockFloor[i].transform.GetChild(j).GetComponent<Block>().Hp = 1;
-            }
-        }
-
+        blockFloor[1].SetBlocksBreakable(true);
+        for(int i = 0; i < blockFloor.Length; i++) {
+            blockFloor[i].SetBlocksHp(1, 1);
+        }       
     }
 
     void Update () {
@@ -48,61 +38,43 @@ public class GameManager : MonoBehaviour {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit2D hit = Physics2D.GetRayIntersection(ray, 100f);
 
-            if(hit.collider != null && hit.transform.CompareTag("Block")) {
+            if (hit.collider && hit.transform.CompareTag("Block")) {
                 Block hitBlock = hit.transform.GetComponent<Block>();
-                if (hitBlock.IsBreakable == true) {
+                BlockFloor hitBlockFloor = hitBlock.GetComponentInParent<BlockFloor>();
+
+                if (hitBlock.IsBreakable) {
                     hitBlock.Hp--;
-                    if(hitBlock.Hp <= 0) {
+
+                    if (hitBlock.Hp <= 0) {
                         hitBlock.gameObject.SetActive(false);
+                        scoreUI.displayscore(1);
 
-                        //UI Score 스코어 점수 1이 올라가게 되어있습니다. 사용하시는 곳에다가 쓰시면 됩니다~ 
-                        scoreUI.displayscore(1);                  
-
-                        for(int i = 0; i < BlockFloor.Length; i++) {
-                            iTween.MoveBy(BlockFloor[i], iTween.Hash("y", -2.00f, "time"
-                                , 1.0f, "delay", 0.5f
-                                ,"oncomplete", "OnMove"
-                                , "oncompletetarget", this.gameObject));
+                        //Moving Blocks
+                        for (int i = 0; i < blockFloor.Length; i++) { 
+                            iTween.MoveBy(blockFloor[i].gameObject, iTween.Hash("y", -2.0f, "time"
+                                , speed, "delay", 0.5f
+                                , "onupdate", "OnMove"
+                                , "onupdatetarget", this.gameObject));
                         }
                     }
                 }
             }
         }
     }
-    void OnMove () {
-        
-        OnBlockBreakablePosition();
-        OnBlockMoveToSpawnPosition();
-    }
-    void OnBlockMoveToSpawnPosition () {
-        Debug.Log("a");
-        for(int i = 0; i < BlockFloor.Length; i++) {
-            int tmpFadePosY = (int)Mathf.Round(FadePos.transform.position.y * 100f);
-            int blockFloorPosY = (int)Mathf.Round(BlockFloor[i].transform.position.y * 100f);
-            BlockFloor[i].transform.position = new Vector3(0, blockFloorPosY * 0.01f, 0);
-            if(tmpFadePosY == blockFloorPosY) {
-                BlockFloor[i].transform.position = SpawnPos.position;
-                for (int j = 0; j < BlockFloor[i].transform.childCount; j++) {
-                    BlockFloor[i].transform.GetChild(j).GetComponent<Block>().IsBreakable = false;
-                    if (!BlockFloor[i].transform.GetChild(j).gameObject.activeInHierarchy) {
-                        BlockFloor[i].transform.GetChild(j).gameObject.SetActive(true);
-                    }
-                }
-            }
-        }
-    }
 
-    void OnBlockBreakablePosition () {
-        Debug.Log("b");
-            for (int i = 0; i < BlockFloor.Length; i++) {
-                int tmpBreakPosY = (int)Mathf.Round(BreakablePos.transform.position.y * 100f);
-                int blockFloorPosY = (int)Mathf.Round(BlockFloor[i].transform.position.y * 100f);
-            BlockFloor[i].transform.position = new Vector3(0, blockFloorPosY * 0.01f, 0);
-            if (tmpBreakPosY == blockFloorPosY) {
-                    for(int j = 0; j < BlockFloor[i].transform.childCount; j++) {
-                    BlockFloor[i].transform.GetChild(j).GetComponent<Block>().IsBreakable = true;
-                    }
-                }
+    void OnMove () {
+        for(int i = 0; i < blockFloor.Length; i++) {
+            //Blocks Move to Spawn Position
+            if (Vector2.Distance(blockFloor[i].transform.position, fadePos.transform.position) < 0.1f) {
+                blockFloor[i].transform.position = spawnPos.position;
+                blockFloor[i].SetBlocksBreakable(false);
+                blockFloor[i].SetBlocksActive(true);
+            }
+
+            //Set Block to be breakable
+            if (Vector2.Distance(blockFloor[i].transform.position, breakablePos.transform.position) < 0.1f) {
+                blockFloor[i].SetBlocksBreakable(true);
             }
         }
+    }
 }
