9caa0b89417a47af5abf8a074e8bae386facef1b
WorkHyeop <WorkHyeop>
2016-08-22 08:14:20 +0900

    InGameScore 수정

--- Assets/06. Scripts/UI/InGameScore.cs
+++ Assets/06. Scripts/UI/InGameScore.cs
@@ -7,7 +7,7 @@ public class InGameScore : MonoBehaviour {
 
    public Text textScore;
    public int totalScore = 0;
-   private int scoreBuffer;
+  
 
 	void Start () {
         if (SceneManager.GetActiveScene().name == "HBJ_PracticeScene")
@@ -16,7 +16,7 @@ public class InGameScore : MonoBehaviour {
         }
         else if (SceneManager.GetActiveScene().name == "GameOverScene")
         {
-            DisplayScore(PlayerPrefs.GetInt("SCORE", scoreBuffer));
+            DisplayScore(PlayerPrefs.GetInt("SCORE", totalScore));
         }
 	}
 
@@ -27,13 +27,13 @@ public class InGameScore : MonoBehaviour {
     public void ScoreSave()
     {
         PlayerPrefs.SetInt("TOTAL_SCORE", totalScore);
-        PlayerPrefs.SetInt("SCORE", scoreBuffer);
+      
     }
     public void DisplayScore(int score)
     {
         totalScore += score;
         textScore.text = totalScore.ToString();
-        scoreBuffer += score;
+    
     }
     
 }
