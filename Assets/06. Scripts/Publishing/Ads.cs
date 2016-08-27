f48130efaf64e034f2cb6ca08f678c60f7ddc420
황보 정 <bomul0327@naver.com>
2016-08-27 18:54:54 +0900

    유니티 Ads 시작

---
+++ Assets/06. Scripts/Publishing/Ads.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class Ads : MonoBehaviour {
+    void Start () {
+        ShowAd();
+    }
+
+    public void ShowAd () {
+        if (Advertisement.IsReady()) {
+            Advertisement.Show();
+        }
+    }
+}
